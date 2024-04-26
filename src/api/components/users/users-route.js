98588da97baac8d3ad8906ba9eafe8a67eb42b6a
NichWes <90470583+NichWes@users.noreply.github.com>
2024-04-26 13:42:02 +0700

    first commit

---
+++ src/api/components/users/users-route.js
@@ -0,0 +1,52 @@
+const express = require('express');
+
+const authenticationMiddleware = require('../../middlewares/authentication-middleware');
+const celebrate = require('../../../core/celebrate-wrappers');
+const usersControllers = require('./users-controller');
+const usersValidator = require('./users-validator');
+
+const route = express.Router();
+
+module.exports = (app) => {
+  app.use('/users', route);
+
+  // Get list of users
+  route.get('/', authenticationMiddleware, usersControllers.getUsers);
+
+  // Get list of users use pagination
+  app.get('/users', (req, res) => {
+    // page_number
+    const page_number = req.query.page_number || 0;
+    const page_size = req.query.page_size || 0;
+  });
+
+  // Create user
+  route.post(
+    '/',
+    authenticationMiddleware,
+    celebrate(usersValidator.createUser),
+    usersControllers.createUser
+  );
+
+  // Get user detail
+  route.get('/:id', authenticationMiddleware, usersControllers.getUser);
+
+  // Update user
+  route.put(
+    '/:id',
+    authenticationMiddleware,
+    celebrate(usersValidator.updateUser),
+    usersControllers.updateUser
+  );
+
+  // Delete user
+  route.delete('/:id', authenticationMiddleware, usersControllers.deleteUser);
+
+  // Change password
+  route.post(
+    '/:id/change-password',
+    authenticationMiddleware,
+    celebrate(usersValidator.changePassword),
+    usersControllers.changePassword
+  );
+};
